a49f1b76628ec02a0470aad7f02d7c5297b26d35
axelzito <axel.ataides@gmail.com>
2019-03-23 11:12:49 -0300

    pino no mapa

--- MyPhomeApp/MapController.swift
+++ MyPhomeApp/MapController.swift
@@ -9,24 +9,58 @@
 import UIKit
 import MapKit
 
-class MapController: UICollectionViewController {
+class MapController:
+    UIViewController,
+    MKMapViewDelegate {
 
+    
+    func mapView(_ mapView: MKMapView, didSelect view: MKAnnotationView) {
+        print("Um pino foi selecionado no mapa")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        vrMap.delegate = self
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        criaPino()
     }
 
+    func criaPino() {
+        let pino = MKPointAnnotation()
+        pino.title = "UFG Mobile"
+        let coordenadas = CLLocationCoordinate2D(latitude: -16.6032579, longitude: -49.2674331)
+        
+        pino.coordinate = coordenadas
+        
+        vrMap.addAnnotation(pino)
+        
+        let zoom = MKCoordinateSpan(latitudeDelta: 0.002, longitudeDelta: 0.002)
+        let regiao = MKCoordinateRegion(center: pino.coordinate, span: zoom)
+        
+        vrMap.setRegion(regiao, animated: true)
+        
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
-    
     @IBOutlet weak var vrMap: MKMapView!
     
-    
     @IBAction func trataEventoSegment(_ sender: UISegmentedControl) {
+        
+        if (sender.selectedSegmentIndex == 0) {
+            vrMap.mapType = .standard
+        } else if (sender.selectedSegmentIndex == 1) {
+            vrMap.mapType = .satellite
+        } else if (sender.selectedSegmentIndex == 2) {
+            vrMap.mapType = .hybrid
+        }
     }
     
     /*
